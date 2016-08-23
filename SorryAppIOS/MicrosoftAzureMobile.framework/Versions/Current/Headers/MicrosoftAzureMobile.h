0056f84078ef287db82903fb5d340399cb708df9
Arvindram Krishnamoorthy <ArvindKrish@D-69-91-143-60.dhcp4.washington.edu>
2016-08-22 18:06:33 -0700

    First push

---
+++ SorryAppIOS/MicrosoftAzureMobile.framework/Versions/Current/Headers/MicrosoftAzureMobile.h
@@ -0,0 +1,33 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+#ifndef MicrosoftAzureMobile_MicrosoftAzureMobile_h
+#define MicrosoftAzureMobile_MicrosoftAzureMobile_h
+
+#import "MSBlockDefinitions.h"
+#import "MSClient.h"
+#import "MSCoreDataStore.h"
+#import "MSDateOffset.h"
+#import "MSError.h"
+#import "MSFilter.h"
+#if TARGET_OS_IPHONE
+#import "MSLoginController.h"
+#endif
+#import "MSPullSettings.h"
+#import "MSPush.h"
+#import "MSQuery.h"
+#import "MSQueryResult.h"
+#import "MSSyncContext.h"
+#import "MSSyncContextReadResult.h"
+#import "MSSyncTable.h"
+#import "MSTable.h"
+#import "MSTableOperation.h"
+#import "MSTableOperationError.h"
+#import "MSUser.h"
+
+#define MicrosoftAzureMobileSdkMajorVersion 3
+#define MicrosoftAzureMobileSdkMinorVersion 0
+#define MicrosoftAzureMobileSdkBuildVersion 0
+
+#endif
